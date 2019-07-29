25f33835ac46ed587f00c68a61ac66ed5140c1f8
daniel <daniellsmarta@gmail.com>
2019-07-29 18:10:24 +0200

    refactoring

--- clientnode/scripts/actions/calc.js
+++ clientnode/scripts/actions/calc.js
@@ -1,36 +1,3 @@
-// const calculator = (client) => {
-//     let formula = document.querySelector('#formula')
-//     let result = document.querySelector('#result')
-//     formula.addEventListener('input', () => {
-//         client.invoke("calc", formula.value, (error, res) => {
-//             if (error) {
-//                 console.error(error)
-//             } else {
-//                 result.textContent = res
-//             }
-//         })
-//     })
-//     formula.dispatchEvent(new Event('input'))
-// }
-
-
-// var calculator = function (client) {
-
-//     let formula = document.querySelector('#formula')
-//     let result = document.querySelector('#result')
-//     formula.addEventListener('input', () => {
-//         client.invoke("calc", formula.value, (error, res) => {
-//             if (error) {
-//                 console.error(error)
-//             } else {
-//                 result.textContent = res
-//             }
-//         })
-//     })
-//     formula.dispatchEvent(new Event('input'))
-
-// }
-
 exports.calculator = function (client) {
 
     let formula = document.querySelector('#formula')
